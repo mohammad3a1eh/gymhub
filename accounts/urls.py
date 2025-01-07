a31e36256f2339c83dda534fb487cd87adf31f8f
Mohammad Saleh <kinitegp@gmail.com>
2025-01-07 12:27:55 +0330

    set new view in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.CustomUserCreationForm.as_view(), name='register'),
+    path('login/', views.custom_login, name='login'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('register/', views.custom_register, name='register'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
 ]
